9b0369a11de602537c7b426fabf7c8346477c52d
RobertoBJ <rbravojuarez15@gmail.com>
2025-02-11 07:48:59 -0600

    Use of modifiers and composable functions

--- app/src/main/java/com/example/trabajoclases/MainActivity.kt
+++ app/src/main/java/com/example/trabajoclases/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.trabajoclases.ui.theme.TrabajoclasesTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,50 +33,61 @@ class MainActivity : ComponentActivity() {
         setContent {
             //creacion de temas
             TrabajoclasesTheme {
-                Row() {
-                    Column() {
-                        Text(text = "Hello user!")
-                        Text(text = "Welcome to your first application!")
+                Column {
+                   Column() {
+                       TextComposable("Roberto")
+                       TextComposable()
+                       TextComposable()
+                       TextComposable()
+                   }
+                    Row() {
+                        TextComposable()
+                        TextComposable()
+                        TextComposable()
+                        TextComposable()
                     }
-                    Column() {
-                        Text(text = "Hello user!")
-                        Text(text = "Welcome to your first application!")
-                    }
-                    Column() {
-                        Text(text = "Hello user!")
-                        Text(text = "Welcome to your first application!")
-                    }
-                    Column() {
-                        Text(text = "Hello user!")
-                        Text(text = "Welcome to your first application!")
+                    Column {
+                        ModiferExamle2()
                     }
                 }
-                /*
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-                 */
             }
         }
     }
-}
-/*
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun TextComposable(name:String = "Empty"){
+        Text("Welcome")
+        Text(name)
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun ModiferExamle1(){
+        Column(modifier = Modifier.padding(50.dp, 30.dp, 20.dp, 10.dp)) {
+            Text("Hello World")
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrabajoclasesTheme {
-        Greeting("Android")
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun ModiferExamle2(){
+        //
+        Column(modifier = Modifier.padding(24.dp).fillMaxWidth().clickable(onClick = { clickAction() })) {
+            Text("Hello World")
+        }
+    }
+    fun clickAction(){
+        println("Column Clicked")
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun ModiferExamle3(){
+        Column(modifier = Modifier.fillMaxHeight().padding(16.dp).background(Color.Cyan)
+            .border(width = 2.dp, color = Color.Black).width(200.dp), horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly) {
+            TextComposable("1")
+            TextComposable("2")
+            TextComposable("3")
+            TextComposable("4")
+        }
     }
 }
- */
